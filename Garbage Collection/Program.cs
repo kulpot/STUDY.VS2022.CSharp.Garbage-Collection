783ae725d165185e67a24f1b2c7f24ef2a833f03
Kulpot <kulpotwahu@gmail.com>
2023-09-07 03:29:18 +0800

    Add project files.

---
+++ Garbage Collection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+//ref link:https://www.youtube.com/watch?v=-JOkkn1ET8c&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=41
+//
+
+class IntWrapper
+{
+    public int wrappedInt;
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        var wrapped1 = new IntWrapper();
+        var wrapped2 = new IntWrapper();
+        var wrapped3 = new IntWrapper();
+        wrapped1.wrappedInt = 1;
+        wrapped2.wrappedInt = 2;
+        wrapped3.wrappedInt = 3;
+        GC.Collect();   // background thread
+        wrapped2 = null;
+        GC.Collect();   // removes wrapped2 heap output then defragments it with something
+    }
+}
